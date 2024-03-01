b3271e8b668870f3843e337e25d1b1c50122fe0a
Abdullah Anli <anliabdullah08@gmail.com>
2024-03-01 18:59:13 +0300

    asd

---
+++ src/main/java/com/EMS/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.EMS.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String surname;
+    private int wage;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname=surname;
+    }
+    public int getWage(){
+        return wage;
+    }
+    public void setWage(int wage){
+        this.wage=wage;
+    }
+}
---
+++ src/main/java/com/EMS/service/EmployeService.java
@@ -0,0 +1,10 @@
+package com.EMS.service;
+
+import com.EMS.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeService {
+    private EmployeeRepository employeeRepository;
+
+}
